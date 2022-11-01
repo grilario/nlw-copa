6d52760b74acf797e38934ab2209fb635dd0516c
Luis Fernando <luisfernandinho094@gmail.com>
2022-11-01 12:13:21 -0300

    class 1

---
+++ mobile/App.tsx
@@ -0,0 +1,52 @@
+import { NativeBaseProvider, StatusBar } from "native-base";
+import { View } from "react-native";
+import { useCallback, useEffect } from "react";
+import * as SplashScreen from "expo-splash-screen";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
+import { SignIn } from "./src/screens/SignIn";
+
+import { THEME } from "./src/styles/theme";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
+
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+        <SignIn />
+      </View>
+    </NativeBaseProvider>
+  );
+}
